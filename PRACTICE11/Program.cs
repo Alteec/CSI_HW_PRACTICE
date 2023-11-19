3b1c98d408157e805f1ad99e64ec06a22b38fe29
Alteec <75168464+Alteec@users.noreply.github.com>
2023-11-19 21:13:42 +0600

    Pracctice 11

---
+++ PRACTICE11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice11
+{
+    class Program
+    {
+        public enum Vacancies{
+            [Display(Name = "Software Engineer")]
+            SoftwareEngineer,
+            [Display(Name = "Product Manager")]
+            ProductManager,
+            [Display(Name = "QA Engineer")]
+            GoodBoy,
+            [Display(Name = "Accountant")]
+            BadBoy
+        }
+
+        struct Employee
+        {
+            public string Name { get; set;}
+            public Vacancies Vacancy { get; set;}
+            DateTime Joined { get; set;}
+            int Salary { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
